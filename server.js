4fe009e70240169e5970394f29fd29cf68b7d635
Billy Bunn <williammbunn@gmail.com>
2019-03-26 15:31:23 -0700

    adds time recorder middleware

--- server.js
+++ server.js
@@ -7,16 +7,20 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const logger = (req, res, next) => {
+const recordTime = (req, res, next) => {
   req.requestTime = moment().format('MM/DD/YYYY [at] h:mm:ss a');
   console.log(req.requestTime);
   next();
+};
+const logger = (req,res,next) => {
+  console.log(req.requestTime);
 };
 
 const notFoundHandler = (req, res, next) => {
   res.status(404).send('Not found');
 };
 
+app.use(recordTime);
 app.use(logger);
 
 app.get('/a', (req, res) => {
