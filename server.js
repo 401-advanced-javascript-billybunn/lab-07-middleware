c077b0f9c60e1ee03fb1291dfb496dfe1f951404
Billy Bunn <williammbunn@gmail.com>
2019-03-26 15:29:14 -0700

    adds logger, 500, and 404 middleware

--- server.js
+++ server.js
@@ -1,25 +1,45 @@
 'use strict';
 
 const express = require('express');
+const moment = require('moment');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/a', (req,res) => {
+const logger = (req, res, next) => {
+  req.requestTime = moment().format('MM/DD/YYYY [at] h:mm:ss a');
+  console.log(req.requestTime);
+  next();
+};
+
+const notFoundHandler = (req, res, next) => {
+  res.status(404).send('Not found');
+};
+
+app.use(logger);
+
+app.get('/a', (req, res) => {
   res.status(200).send('Route A');
 });
 
-app.get('/b', (req,res) => {
+app.get('/b', (req, res) => {
   res.status(200).send('Route B');
 });
 
-app.get('/c', (req,res) => {
+app.get('/c', (req, res) => {
   res.status(200).send('Route C');
 });
 
-app.get('/d', (req,res) => {
+app.get('/d', (req, res) => {
   res.status(200).send('Route D');
+});
+
+app.use('*', notFoundHandler);
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  res.status(500).send('There was a server error');
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
